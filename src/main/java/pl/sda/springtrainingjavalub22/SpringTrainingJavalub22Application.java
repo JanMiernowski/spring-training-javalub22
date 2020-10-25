3f4a4a0da5ef46ed53e65ba35e3de154894b3244
Tomasz.Rutyna <Poniatowa2021!>
2020-10-25 15:22:39 +0100

    scheduled

--- src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
+++ src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
@@ -3,10 +3,12 @@ package pl.sda.springtrainingjavalub22;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.sda.springtrainingjavalub22.config.CompanyInfo;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CompanyInfo.class})
+@EnableScheduling
 public class SpringTrainingJavalub22Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/sda/springtrainingjavalub22/domain/car/CarTask.java
@@ -0,0 +1,33 @@
+package pl.sda.springtrainingjavalub22.domain.car;
+
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class CarTask {
+
+    private CarService carService;
+
+    @Scheduled(cron = "0 21 15 * * ?")
+    public void checkInsurance() {
+        List<Car> all = carService.getAll();
+
+        all.stream()
+                .filter(this::isInsuranceCloseToEnd)
+                .forEach(car -> System.out.println(
+            String.format("Dla auta o VIN %s ubezpieczenie konczy sie dnia %s",
+                car.getVin(), car.getInsuredTo().toString())));
+    }
+
+    private boolean isInsuranceCloseToEnd(Car car) {
+        LocalDate now = LocalDate.now();
+        return car.getInsuredTo().isBefore(now) ||
+                ChronoUnit.DAYS.between(now, car.getInsuredTo()) < 10;
+    }
+}
