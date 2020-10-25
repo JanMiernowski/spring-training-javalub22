d0a76ec248e8cc977329d3086ea545fc59eb5bf9
Tomasz.Rutyna <Poniatowa2021!>
2020-10-25 10:22:15 +0100

    thymleaf - first part

---
+++ src/main/java/pl/sda/springtrainingjavalub22/web/CarController.java
@@ -0,0 +1,28 @@
+package pl.sda.springtrainingjavalub22.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.springtrainingjavalub22.domain.car.CarService;
+
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("mvc/car")
+@AllArgsConstructor
+public class CarController {
+
+    private CarService carService;
+
+    @GetMapping
+    ModelAndView displayCarsPage() {
+        ModelAndView mav = new ModelAndView("cars.html");
+        mav.addObject("cars", carService.getAll());
+        mav.addObject("todayDate", LocalDate.now());
+
+        return mav;
+    }
+
+}
