6b22bdda174a17d40eb9c27a3221f69d48cb6950
Tomasz.Rutyna <Poniatowa2021!>
2020-11-08 13:23:22 +0100

    integration test - exercise

--- src/test/java/pl/sda/springtrainingjavalub22/domain/car/CarServiceITTest.java
+++ src/test/java/pl/sda/springtrainingjavalub22/domain/car/CarServiceITTest.java
@@ -1,9 +1,12 @@
 package pl.sda.springtrainingjavalub22.domain.car;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.sda.springtrainingjavalub22.api.model.SearchParams;
 import pl.sda.springtrainingjavalub22.exception.AlreadyExistException;
 import pl.sda.springtrainingjavalub22.external.car.CarEntity;
 import pl.sda.springtrainingjavalub22.external.car.InsuranceEntity;
@@ -20,6 +23,57 @@ public class CarServiceITTest {
 
     @Autowired
     private JpaCarRepository jpaCarRepository;
+
+    @BeforeEach
+    public void cleanDb() {
+        jpaCarRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenNoCarsExistInDb() {
+        //when
+        List<Car> cars = carService.searchByParams(new SearchParams());
+        //then
+        Assertions.assertTrue(cars.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnByGivenManufacturer() {
+        //given
+        CarEntity carEntity1 = new CarEntity(null, "VW", "Golf", 2010,
+                "Vin1", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        CarEntity carEntity2 = new CarEntity(null, "Skoda", "Golf", 2011,
+                "Vin2", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        CarEntity carEntity3 = new CarEntity(null, "VW", "Passat", 2014,
+                "Vin3", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        jpaCarRepository.saveAll(Lists.list(carEntity1, carEntity2, carEntity3));
+        SearchParams searchParams = new SearchParams();
+        searchParams.setManufacturer("VW");
+        //when
+        List<Car> carList = carService.searchByParams(searchParams);
+        //then
+        Assertions.assertEquals(2, carList.size());
+        Assertions.assertTrue(carList.stream().allMatch(car -> car.getManufacturer().equals("VW")));
+    }
+
+    @Test
+    public void shouldReturnOnlyNewerThanGivenProductionYear() {
+        //given
+        CarEntity carEntity1 = new CarEntity(null, "VW", "Golf", 2010,
+                "Vin1", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        CarEntity carEntity2 = new CarEntity(null, "Skoda", "Golf", 2011,
+                "Vin2", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        CarEntity carEntity3 = new CarEntity(null, "VW", "Passat", 2014,
+                "Vin3", new InsuranceEntity(null, LocalDate.now(), LocalDate.now().plusMonths(6)));
+        jpaCarRepository.saveAll(Lists.list(carEntity1, carEntity2, carEntity3));
+        SearchParams searchParams = new SearchParams();
+        searchParams.setProductionFrom(2012);
+        //when
+        List<Car> cars = carService.searchByParams(searchParams);
+        //then
+        Assertions.assertEquals(1, cars.size());
+        Assertions.assertTrue(cars.stream().allMatch(car -> car.getYearOfProduction() > 2012));
+    }
 
     @Test
     public void shouldPersistCarInDb() {
