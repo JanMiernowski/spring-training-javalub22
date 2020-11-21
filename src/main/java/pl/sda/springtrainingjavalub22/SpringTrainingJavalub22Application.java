01f1a31ba767133998d10428620270b91656de46
Tomasz.Rutyna <Poniatowa2021!>
2020-11-21 14:48:31 +0100

    swagger ui

--- src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
+++ src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.sda.springtrainingjavalub22.config.CompanyInfo;
 import pl.sda.springtrainingjavalub22.config.SmtpProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CompanyInfo.class, SmtpProperties.class})
 @EnableScheduling
+@EnableSwagger2
 public class SpringTrainingJavalub22Application {
 
     public static void main(String[] args) {
